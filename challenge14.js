132c8d3d6575870eae017678bdf5c5d470e391a7
Gaurav Pratap Singh <gs492979@gmail.com>
2025-05-12 23:08:56 +0530

    c14 code updated.

--- challenge14.js
+++ challenge14.js
@@ -1,17 +1,18 @@
 // Wap to Count the no. of even and odd digits of number entered by the user.
-let originalNum = Number(prompt("Enter any no. to count its digit"));
-let num = originalNum;
-let evenCount = 0;
-let oddCount = 0;
-while (num > 0) {
-  let digit = num % 10;
-  if (digit % 2 == 0) {
-    evenCount++;
-  } else {
-    oddCount++;
+function countEvenOddDigit(originalNum) {
+  let num = originalNum;
+  let evenCount = 0;
+  let oddCount = 0;
+  while (num > 0) {
+    let digit = num % 10;
+    if (digit % 2 == 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+    num = Math.floor(num / 10);
   }
-  num = Math.floor(num / 10);
+  return `The number ${originalNum} has ${evenCount} even digits and ${oddCount} odd digits.`;
 }
-console.log(
-  `The number ${originalNum} has ${evenCount} even digits and ${oddCount} odd digits.`
-);
+
+console.log(countEvenOddDigit(2344));
